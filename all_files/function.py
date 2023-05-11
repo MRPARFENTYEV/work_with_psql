0b553ac8b79a24c943c63269511102b6eaa46b36
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-05-12 00:03:23 +0300

    Rework after sanding out

--- all_files/function.py
+++ all_files/function.py
@@ -1,99 +1,94 @@
 import psycopg2
-from config import HOST, PORT, USER,PASSWORD, DATABASENAME
+from config import HOST, PORT, USER, PASSWORD, DATABASENAME
+
+conn = psycopg2.connect(database=DATABASENAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
+
 
 # Функция, создающая структуру БД (таблицы).
-def create_tables(conn):
-
-    with conn.cursor() as cursor:
-        # Попытка выполнение транзации
-
-        cursor.execute('create table username(id bigint generated always as identity primary key,'
-                       'name VARCHAR(50), surname varchar(50), email varchar(100))')
-        cursor.execute('create table phone(id bigint generated always as identity primary key,'
-                       'phone VARCHAR(20))')
-        cursor.execute('create table username_phone(user_id bigint references username(id),'
-                       'phone_id bigint references phone(id))')
-
+# def create_tables(conn):
+#
+#     with conn.cursor() as cursor:
+#         # Попытка выполнение транзации
+#
+#         cursor.execute('create table username(id bigint generated always as identity primary key,'
+#                        'name VARCHAR(50), surname varchar(50), email varchar(100))')
+#         cursor.execute('create table phone(id bigint generated always as identity primary key,'
+#                        'phone VARCHAR(20))')
+#
+#         cursor.execute('ALTER TABLE phone ADD user_id bigint references username(id)')
+#         conn.commit()
+# # create_tables(conn)
+#
 # Функция, позволяющая добавить нового клиента.
-def create_user(user_data: list, conn):
-    with conn.cursor() as cursor:
-        # Попытка выполнение транзации
-
-        cursor.execute( 'INSERT INTO username( name, surname, email) VALUES (%s,%s,%s)',(user_data[0],user_data[1],user_data[2]))
-
-        # cursor.execute(f"INSERT INTO phone(phone) VALUES ('{user_data[3]}')")
-
-        conn.commit()
-        print("Данные добавлены")
-
-create_user(['Варений','Mеладзе','@','123321'],conn)
-create_user(['Jack','London','@.GB','1234321'],conn)
-
-
+# def create_user(user_data: list, conn):
+#     with conn.cursor() as cursor:
+#         # Попытка выполнение транзации
+#
+#         cursor.execute( 'INSERT INTO username( name, surname, email) VALUES (%s,%s,%s)',(user_data[0],user_data[1],user_data[2]))
+#
+#         conn.commit()
+#         print("Данные добавлены")
+#
+# create_user(['Варений','Mеладзе','@'],conn)
+# # create_user(['Jack','London','@.GB'],conn)
+#
+#
 # функция, позволяющая добавить телефон для существующего клиента
-def insert_phone(conn,phone,user_id):
-    with conn.cursor() as cursor:
-        cursor.execute(f"INSERT INTO phone(phone) VALUES ('{phone}')")
-        '''вставляем телефон'''
-        cursor.execute(f"select id from phone where phone = '{phone}'")
-        cursor.execute('INSERT INTO username_phone(user_id,phone_id) values (%s,%s)',(user_id, cursor.fetchall()[0][0]))
-        '''нашли id телефона из phone'''
-        conn.commit()
-        print("Данные добавлены")
-insert_phone(conn,'799090909','1')
-
-
+# def insert_phone(conn,phone,user_id):
+#     with conn.cursor() as cursor:
+#         cursor.execute(f"INSERT INTO phone(phone,user_id) VALUES ('{phone}','{user_id}')")
+#
+#         conn.commit()
+#         print("Данные добавлены")
+# insert_phone(conn,'89036888888',2)
+# #
+#
 # функция, которая позволяет изменить данные о клиенте
-def change_user_name(conn, name, surname, mail, phone, user_id, phone_id):
-    with conn.cursor() as cursor:
-        cursor.execute("UPDATE username SET name =%s, surname = %s, email = %s where id = %s;",
-                       (name, surname, mail, user_id))
-        cursor.execute("UPDATE phone SET phone =%s where id =%s", (phone, phone_id))
-
-
-        conn.commit()
-        print("Данные добавлены")
-change_user_name(conn,'Alfred','Waune','@blist','8800555','10','17')
-
-
+# def change_user_name(conn, user_id,phone_id, name=None, surname=None, mail=None,phone=None ):
+#     with conn.cursor() as cursor:
+#         if name:
+#            cursor.execute("UPDATE username SET name =%s where id =%s;",(name,user_id))
+#
+#         if surname:
+#             cursor.execute("UPDATE username SET surname =%s where id =%s;",(surname,user_id))
+#
+#         if mail:
+#             cursor.execute("UPDATE username SET mail =%s where id =%s;",(mail,user_id))
+#
+#         if phone:
+#             cursor.execute("UPDATE phone SET phone =%s where id =%s;", (phone, phone_id))
+#
+#         conn.commit()
+#         print("Данные добавлены")
+#
+#
+#
+#
+# change_user_name(conn,'1','4',name='Pavel' )
+#
+#
 # Функция, позволяющая удалить телефон для существующего клиента.
-def del_user_phone(conn,phone_id):
-    with conn.cursor() as cursor:
-        cursor.execute(f"DELETE FROM username_phone where phone_id = '{phone_id}' ")
-        cursor.execute(f"DELETE FROM phone where id = '{phone_id}' ")
-        conn.commit()
-del_user_phone(conn, 14)
+# def del_user_phone(conn,id_user):
+#     with conn.cursor() as cursor:
+#         cursor.execute(f"DELETE FROM phone where user_id = '{id_user}' ")
+#         conn.commit()
+# del_user_phone(conn, 1)
 
-
+#
 # Функция, позволяющая удалить существующего клиента
-def del_user(user_id):
-    with conn.cursor() as cursor:
-
-        '''удалили связь между таблицами'''
-        cursor.execute("ALTER TABLE username_phone  DROP CONSTRAINT username_phone_user_id_fkey")
-        cursor.execute("ALTER TABLE username_phone  DROP CONSTRAINT username_phone_phone_id_fkey")
-
-        '''выполнили каскадное удаление из user_phone до phone'''
-        cursor.execute(f"DELETE FROM phone WHERE id IN (SELECT phone_id FROM username_phone where user_id = '{user_id}') ")
-        cursor.execute(f"DELETE FROM username where id = '{user_id}' ")
-        cursor.execute(f"DELETE FROM username_phone where user_id = '{user_id}' ")
-
-        '''восстановили связь между таблицами'''
-        cursor.execute(
-            "ALTER TABLE username_phone ADD CONSTRAINT username_phone_user_id_fkey FOREIGN KEY (user_id) "
-        "REFERENCES username(id)")
-        cursor.execute(
-            "ALTER TABLE username_phone ADD CONSTRAINT username_phone_phone_id_fkey FOREIGN KEY (phone_id) "
-            "REFERENCES phone(id)"
-        )
-        conn.commit()
-    print("Данные удалены")
-
-del_user(1)
-
-
+# def del_user(user_id):
+#     with conn.cursor() as cursor:
+#
+#         cursor.execute(f"DELETE FROM phone WHERE (user_id = '{user_id}') ")
+#         cursor.execute(f"DELETE FROM username where (id = '{user_id}') ")
+#         conn.commit()
+#     print("Данные удалены")
+#
+# del_user(1)
+# #
+#
 # Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
-def select_user(name=None,surname=None, email=None, phone=None):
+def select_user(name=None,surname=None,email=None,phone=None):
     if name:
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM username where name = '{name}' ")
@@ -108,7 +103,7 @@ def select_user(name=None,surname=None, email=None, phone=None):
         print(cursor.fetchall())
     if phone:
         cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM username LEFT JOIN username_phone on username_phone.user_id = username.id WHERE phone_id IN(SELECT id FROM phone where phone = '{phone}')")
+        cursor.execute(f"SELECT * FROM username LEFT JOIN phone on username.id = phone.user_id WHERE phone = '{phone}'")
 
         print(cursor.fetchall())
-print(select_user(phone='1234321'))
+select_user(phone='89036888888')
--- all_files/main.py
+++ all_files/main.py
@@ -1,8 +1,8 @@
 import psycopg2
 
-from config import HOST, PORT, USER,PASSWORD, DATABASENAME
+from config import HOST, PORT, USER, PASSWORD, DATABASENAME
 
-conn = psycopg2.connect(database =DATABASENAME, user = USER, password = PASSWORD,host = HOST, port = PORT)
+conn = psycopg2.connect(database=DATABASENAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
 with conn.cursor() as cursor:
     # Попытка выполнение транзации
     try:
@@ -11,7 +11,7 @@ with conn.cursor() as cursor:
         cursor.execute('create table phone(id bigint generated always as identity primary key,'
                        'phone VARCHAR(20))')
         cursor.execute('create table username_phone(user_id bigint references username(id),'
-        'phone_id bigint references phone(id))')
+                       'phone_id bigint references phone(id))')
 
 
 
@@ -30,3 +30,55 @@ with conn.cursor() as cursor:
         # save result of transaction
         print("Successful transaction")
 
+
+def insert_phone(conn, phone, user_id):
+    with conn.cursor() as cursor:
+        cursor.execute(f"INSERT INTO phone(phone) VALUES ('{phone}')")
+        '''вставляем телефон'''
+        cursor.execute(f"select id from phone where phone = '{phone}'")
+        cursor.execute('INSERT INTO username_phone(user_id,phone_id) values (%s,%s)',
+                       (user_id, cursor.fetchall()[0][0]))
+        '''нашли id телефона из phone'''
+        conn.commit()
+        print("Данные добавлены")
+
+
+insert_phone(conn, '799090909', '1')
+
+
+#
+
+def change_user_name(conn, name=None, surname=None, mail=None, phone=None, user_id, phone_id):
+    with conn.cursor() as cursor:
+
+
+cursor.execute("UPDATE username SET name =%s, surname = %s, email = %s where id = %s;",
+               (name, surname, mail, user_id))
+cursor.execute("UPDATE phone SET phone =%s where id =%s", (phone, phone_id))
+
+conn.commit()
+print("Данные добавлены")
+
+
+def del_user(user_id):
+    with conn.cursor() as cursor:
+
+        '''удалили связь между таблицами'''
+        cursor.execute("ALTER TABLE username_phone  DROP CONSTRAINT username_phone_user_id_fkey")
+        cursor.execute("ALTER TABLE username_phone  DROP CONSTRAINT username_phone_phone_id_fkey")
+
+        '''выполнили каскадное удаление из user_phone до phone'''
+        cursor.execute(f"DELETE FROM phone WHERE id IN (SELECT phone_id FROM username_phone where user_id = '{user_id}') ")
+        cursor.execute(f"DELETE FROM username where id = '{user_id}' ")
+        cursor.execute(f"DELETE FROM username_phone where user_id = '{user_id}' ")
+
+        '''восстановили связь между таблицами'''
+        cursor.execute(
+            "ALTER TABLE username_phone ADD CONSTRAINT username_phone_user_id_fkey FOREIGN KEY (user_id) "
+        "REFERENCES username(id)")
+        cursor.execute(
+            "ALTER TABLE username_phone ADD CONSTRAINT username_phone_phone_id_fkey FOREIGN KEY (phone_id) "
+            "REFERENCES phone(id)"
+        )
+        conn.commit()
+    print("Данные удалены")
